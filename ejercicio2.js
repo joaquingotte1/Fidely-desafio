572bcb8609000793260b06a20275efe072fa81d5
joaquingotte1 <107966621+joaquingotte1@users.noreply.github.com>
2022-06-21 21:01:29 -0300

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,5 @@
+let uno = [1, 2, 3, 4, 5, 7]
+let dos = [];
+for (let i = 0; i < uno.length; i++)
+    dos.push(uno[i] + 1)
+console.log(dos)
---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+var array = [1, 10, 5, 100];
+
+array.sort(function(a, b) {
+    return a - b;
+});
+
+alert(array[0]);
+alert(array[1]);
+alert(array[2]);
+alert(array[3]);
